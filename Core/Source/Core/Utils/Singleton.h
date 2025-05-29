1897afbfe57cec2743e10c2467126a8a7d5aae34
LuckyGhostStudio <1398346167@qq.com>
2025-05-30 00:30:55 +0800

    添加泛型单例基类Singleton

---
+++ Core/Source/Core/Utils/Singleton.h
@@ -0,0 +1,50 @@
+#pragma once
+
+namespace LFramework
+{
+    /// <summary>
+    /// 单例基类
+    /// </summary>
+    /// <typeparam name="T">类型</typeparam>
+    template<typename T>
+    class Singleton
+    {
+    protected:
+        Singleton() = default;
+        virtual ~Singleton() = default;
+    public:
+        Singleton(const Singleton&) = delete;
+        Singleton& operator=(const Singleton&) = delete;
+
+        static void Create()
+        {
+            // LF_ASSERT(s_Instance == nullptr);
+
+            if (!s_Instance)
+            {
+                s_Instance = new T;
+            }
+        }
+
+        static T& Get()
+        {
+            // LF_ASSERT(s_Instance != nullptr)
+
+            return *s_Instance;
+        }
+
+        static void Destroy()
+        {
+            if (s_Instance)
+            {
+                delete s_Instance;
+                s_Instance = nullptr;
+            }
+        }
+    private:
+        static T* s_Instance;
+    };
+
+    template<typename T>
+    T* Singleton<T>::s_Instance = nullptr;
+}
